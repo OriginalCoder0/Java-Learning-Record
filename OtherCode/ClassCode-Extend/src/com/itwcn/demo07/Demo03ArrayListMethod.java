3506a2f94b348826efd1f54777cdb1a304078b0f
OriginalCoder0 <1720929001@qq.com>
2020-11-13 11:12:06 +0800

    :new:更新代码

---
+++ OtherCode/ClassCode-Extend/src/com/itwcn/demo07/Demo03ArrayListMethod.java
@@ -0,0 +1,23 @@
+package com.itwcn.demo07;
+
+/**
+ * @program: Demo03ArrayListMethod
+ * @description:
+ * @author: OriginalCoder
+ * @createtime:2020/11/11 9:53
+ **/
+
+/**
+ * ArrayList当中常用的方法有：
+ * public boolean add(E e):向集合中添加元素，参数的类型和泛型一致。
+ * public E get(int index)：从集合中获取元素，参数是索引编号，返回值就是对应位置的元素。
+ * public E remove(int index):从集合中删除元素，参数是索引编号，返回值就是被删除掉的元素
+ * public int size:获取集合的尺寸长度，返回值是集合中包含的元素个数
+ * @author 王佳涵
+ */
+public class Demo03ArrayListMethod {
+    public static void main(String[] args) {
+
+    }
+
+}
